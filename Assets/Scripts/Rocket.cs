66a86b46db1af3df10485c9f8ce6e37ebbf56ec8
Eldarian <eldarian3@gmail.com>
2020-12-07 15:12:52 +0300

    Lection10 Added Rocket

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    private float rotationSpeed = 100;
+    [SerializeField] private float force = 50;
+    private Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Space pressed");
+           rigidbody.AddRelativeForce(Vector3.up * force, ForceMode.Force);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotating left");
+            transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotating right");
+            transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime * -1);
+
+        }
+    }
+}
