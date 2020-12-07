87a88ecbda60490a7be5d139191aee512cddcfb4
Eldarian <eldarian3@gmail.com>
2020-12-07 16:00:49 +0300

    Lection14 Audio

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -8,11 +8,13 @@ public class Rocket : MonoBehaviour
     private float rotationSpeed = 100;
     [SerializeField] private float force = 50;
     private Rigidbody rigidbody;
+    AudioSource audio;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        audio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -26,8 +28,18 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             print("Space pressed");
-           rigidbody.AddRelativeForce(Vector3.up * force, ForceMode.Force);
+            rigidbody.AddRelativeForce(Vector3.up * force, ForceMode.Force);
+            if (!audio.isPlaying)
+            {
+                audio.Play();
+            }
+            
         }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            audio.Stop();
+        }
+
         if (Input.GetKey(KeyCode.A))
         {
             print("Rotating left");
